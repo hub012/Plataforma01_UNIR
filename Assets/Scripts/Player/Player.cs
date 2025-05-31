51c74ab2c3f375ed9f73bc9614002be8088d64c7
hub012 <hub0121@gmail.com>
2025-05-31 04:50:44 -0400

    Refactor and comments

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,96 +5,102 @@ namespace Player
 {
     public class Player : MonoBehaviour
     {
-    
-        public Rigidbody2D playerRigidbody;
-        [SerializeReference] public float speed;
-        [SerializeReference] public float jumpSpeed;
-        [SerializeField] private LayerMask groundLayer;
-
-        public float SprintingSpeed { get; private set; }
-        public Animator playerAnimator;
+        [Header("Movement Settings")]
+        public float speed = 5f;
+        public float jumpSpeed = 10f;
+        public float sprintMultiplier = 2f;
         
-        private float distanceToGround;
-        public bool IsAirborne { get; set; } 
-
+        [Header("Ground Detection")]
+        [SerializeField] private LayerMask groundLayer;
+        [SerializeField] private float groundCheckDistance = 0.1f;
+        
+        // Components
+        public Rigidbody2D playerRigidbody { get; private set; }
+        public Animator playerAnimator { get; private set; }
+        public PlayerControls PlayerControls { get; private set; }
+        
+        // Properties
+        public float SprintingSpeed => speed * sprintMultiplier;
+        public bool IsAirborne { get; set; }
+        
+        // State Machine
         private PlayerStateMachine _playerStateMachine;
-        private PlayerState currentState;
-    
+        
         #region Player States
-        public WalkState walkState;
-        public IdleState idleState;
-        public RunState runState;
-        public JumpState jumpState;
-        public VerticalAttackState verticalAttackState;
+        public WalkState walkState { get; private set; }
+        public IdleState idleState { get; private set; }
+        public RunState runState { get; private set; }
+        public JumpState jumpState { get; private set; }
+        public VerticalAttackState verticalAttackState { get; private set; }
         #endregion
-        #region Player Controls
-        public PlayerControls PlayerControls{ get; private set;}
-        #endregion
+
+        void Awake()
+        {
+            // Get components
+            playerRigidbody = GetComponent<Rigidbody2D>();
+            playerAnimator = GetComponent<Animator>();
+            PlayerControls = GetComponent<PlayerControls>();
+            
+            // Initialize state machine
+            _playerStateMachine = new PlayerStateMachine();
+            
+            // Initialize states
+            InitializeStates();
+        }
 
         void Start()
         {
-            // Inputs
-            PlayerControls = GetComponent<PlayerControls>(); 
+            // Start with idle state
+            _playerStateMachine.InitStateMachine(idleState);
+        }
 
-            //speed init
-            SprintingSpeed = speed * 2;
-            playerRigidbody = GetComponent<Rigidbody2D>();
-            playerAnimator = GetComponent<Animator>();
-            _playerStateMachine = new PlayerStateMachine();
+        void Update()
+        {
+            // Update current state
+            _playerStateMachine.CurrentState?.LogicUpdate();
+            
+            // Handle sprite flipping
+            FlipSprite();
+            
+            // Debug info (remove in production)
+            Debug.Log($"Current State: {_playerStateMachine.CurrentState?.GetType().Name}");
+            Debug.Log($"Is Grounded: {IsGrounded()}");
+        }
 
-            distanceToGround = GetComponent<Collider2D>().bounds.extents.y;
+        void FixedUpdate()
+        {
+            // Physics updates for current state
+            _playerStateMachine.CurrentState?.PhysicsUpdate();
+        }
 
-            // Set IdleState as the initial state
-            currentState = idleState = new IdleState(this, _playerStateMachine, playerAnimator);
+        private void InitializeStates()
+        {
+            idleState = new IdleState(this, _playerStateMachine, playerAnimator);
             walkState = new WalkState(this, _playerStateMachine, playerAnimator);
             runState = new RunState(this, _playerStateMachine, playerAnimator);
             jumpState = new JumpState(this, _playerStateMachine, playerAnimator);
             verticalAttackState = new VerticalAttackState(this, _playerStateMachine, playerAnimator);
-            _playerStateMachine.InitStateMachine(currentState); //Inicio la maquina con todos los estados predefenidos
-    
         }
 
-        // Update is called once per frame
-        void Update()
+        private void FlipSprite()
         {
-            Debug.Log("Jump " + PlayerControls.JumpPressed);
-            _playerStateMachine?.CurrentState?.LogicUpdate();
-            // Detectar si el jugador tiene la mas minima velocidad y si la ultima velocidad que tuvo fue negativa o positiva
-            //bool playerHasMovementSpeed = Mathf.Abs(playerRigidbody.velocity.x) > Mathf.Epsilon;
-
-            FlipSprite();
-            Debug.Log("Este en el suelo "+IsGrounded());
-            Debug.Log("Distancia al suelo: "+distanceToGround);
-
-        
-            //if (!playerHasMovementSpeed)
-        }
-        void FixedUpdate(){
-            _playerStateMachine?.CurrentState?.PhysicsUpdate();
-        }
-
-        /// <summary>
-        /// Method <c>FlipSprite</c> Hay un truco para hacer flip del player con cambiar el scale entre -1 y 1 en el eje x
-        /// Uso Mathf.Sign para solo sacar si el valor positivo o no y usarlo en base al 1
-        /// Si es 0 va hacia el valor positivo es decir uno.
-        /// Ejemplos: Mathf.Sign(35) = 1, Mathf.Sign(-40) = -1, Mathf.Sign(0) = 1
-        /// </summary>
-        void FlipSprite(){
             if (Mathf.Abs(PlayerControls.inputMove.x) > 0.01f)
             {
-                float direction = Mathf.Sign(PlayerControls.inputMove.x); 
-                transform.localScale = new Vector2(direction, 1f);
+                float direction = Mathf.Sign(PlayerControls.inputMove.x);
+                transform.localScale = new Vector3(direction, 1f, 1f);
             }
         }
 
         public bool IsGrounded()
         {
-            float rayLength = distanceToGround + .6f;
-            Vector2 origin = transform.position;
+            Vector2 origin = (Vector2)transform.position + Vector2.down * 0.5f;
+            float rayLength = groundCheckDistance;
+            
             RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, rayLength, groundLayer);
-
-            Debug.DrawRay(origin, Vector2.down * rayLength, Color.red); // Ver en Scene
-
+            
+            // Debug visualization
+            Debug.DrawRay(origin, Vector2.down * rayLength, hit.collider != null ? Color.green : Color.red);
+            
             return hit.collider != null;
         }
     }
